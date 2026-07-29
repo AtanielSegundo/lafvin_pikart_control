cc76de5a7a4a34186ed412b15568921fa558a241
ataniel <10ataniel@gmail.com>
2026-07-29 18:58:57 -0300

    Real track distance calculated

--- Server/config.py
+++ Server/config.py
@@ -25,7 +25,7 @@ from typing import Dict, Tuple
 class WheelGeometry:
     diameter: float = 0.065            # m
     colinear_distance: float = 0.095   # m, between motors on the same axle
-    track: float = 0.202               # m, distance between left and right sides
+    track: float = 0.155               # m, distance between left and right sides
     counts_per_rev: int = 2340        
                                       # (quadrature x4). CALIBRATE for your build.
 
